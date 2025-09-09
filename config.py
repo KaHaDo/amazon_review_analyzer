6abb79475513c7ec0aa22b5892b01a8336cc0604
KaHaDo <dominik.boschner@iu-study.org>
2025-09-09 19:54:13 +0200

    Upload config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+import yaml
+
+_config = None
+
+def load_config(path='config.yaml'):
+	global _config
+	if _config is None:
+		with open(path, 'r') as f:
+			_config = yaml.safe_load(f)
+	return _config
